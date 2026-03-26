a15ecb3ac00205f5ae848317c5a6dcd930e2a3e9
GemmaRyan <s00250864@atu.ie>
2026-03-26 17:50:25 +0000

    changes

--- src/index.ts
+++ src/index.ts
@@ -18,9 +18,7 @@ app.use(express.json());
 
 
 app.use(cors({
-  origin: ['http://localhost:4200',
-    'https://recipe-app-frontend-jy3h.onrender.com'
-  ],
+  origin: 'https://recipe-app-frontend-jy3h.onrender.com',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
