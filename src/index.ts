581c0524e25d248c5ea22cd5e4501b7e95e00a72
GemmaRyan <s00250864@atu.ie>
2026-03-26 17:01:28 +0000

    changed frontend endpoint

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ app.use(express.json());
 
 app.use(cors({
   origin: ['http://localhost:4200',
-    'https://recipe-app-frontend-4qko.onrender.com'
+    'https://recipe-app-frontend-jy3h.onrender.com'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
