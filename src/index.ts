05bd9265d2ac0a1e4cca5fecf46f6567cadc2d46
GemmaRyan <s00250864@atu.ie>
2026-03-26 16:34:49 +0000

    Small changes for the deployment -need to make more changes

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,9 @@ app.use(express.json());
 
 
 app.use(cors({
-  origin: 'http://localhost:4200',
+  origin: ['http://localhost:4200',
+    'https://recipe-app-frontend-4qko.onrender.com'
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
